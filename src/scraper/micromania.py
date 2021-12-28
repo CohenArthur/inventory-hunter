33f7e3a6093a83bd92db4355ed11df2c56e86aa9
CohenArthur <cohenarthur.dev@gmail.com>
2021-12-29 00:53:21 +0100

    micromania: Add Micromania scrapper

--- src/scraper/micromania.py
+++ src/scraper/micromania.py
@@ -5,14 +5,21 @@ class MicroManiaResult(ScrapeResult):
         alert_subject = 'In Stock'
         alert_content = ''
 
-        # Attempt to find the "Alert me" button
-        button = self.soup.body.find("div", { "class": "add-to-cart-container d-none" })
-        if button:
+        button = None
+
+        divs = self.soup.body.find_all("div", class_="add-to-cart-container d-none")
+        if len(divs) != 0:
+            button = divs[0]
             return
 
-        button = self.soup.body.find("div", { "class": "add-to-cart-container " })
-        if button:
-            price_str = self.set_price("1640.00")
+        divs = self.soup.body.find_all("div", class_="add-to-cart-container")
+        if len(divs) != 0:
+            spans = self.soup.body.find_all("span", { "itemprop": "price"}, class_="value")
+            if len(spans) != 0:
+                price = spans[0]["content"]
+                price_str = self.set_price(price)
+            else:
+                return
             self.alert_subject = alert_subject
             self.alert_content = f"Wouuuuuuh {price_str}"
 
